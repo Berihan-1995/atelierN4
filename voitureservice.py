771c98ed3f0cc7b57f21a59e72cad0558a62df0e
berihqn mdcherif <3001555242@monboreal.ca>
2026-03-10 23:07:14 -0400

    affiche information employe

--- voitureservice.py
+++ voitureservice.py
@@ -5,3 +5,12 @@ class Employe:
         self.prenom = prenom
         self.voitureService=None
 
+    def afficherInformations(self):
+        print(f"Permis : {self.numeroPermis}")
+        print(f"Nom : {self.nom}")
+        print(f"Prenom : {self.prenom}")
+        if self.voitureService != None:
+            print("voiture de service")
+            self.voitureService.afficherInformations()
+        else:
+            print("il n'est pas voiture de service")
